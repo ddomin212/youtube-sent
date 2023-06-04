043c35fd33138cce6fd5f30ed9891008b6969f7e
ddomin212 <68774450+ddomin212@users.noreply.github.com>
2023-06-04 18:28:57 +0200

    docstrings again

--- functions/kaggle/get_sentiment.py
+++ functions/kaggle/get_sentiment.py
@@ -1,11 +1,18 @@
+"""
+Handles sentiment analysis for YouTube video comments.
+
+Functions:
+    get_sentiment: Given a list of comments on a YouTube video, 
+    returns the sentiment of each comment using the Kaggle API.
+"""
 import subprocess
 import pandas as pd
-import subprocess
 
 
 def get_sentiment(comments, folder_name, first_time):
     """
-    Given a the comments on a YouTube video, return comments tagged with sentiment and their respective counts.
+    Given a the comments on a YouTube video, 
+    return comments tagged with sentiment and their respective counts.
 
     Args:
         text (str): The text to analyze.
@@ -14,11 +21,12 @@ def get_sentiment(comments, folder_name, first_time):
         str: The sentiment of the text.
     """
     # run the bash script
-    subprocess.run(['bash', 'functions/kaggle/kaggle.sh', folder_name, first_time])
+    subprocess.run(['bash', 'functions/kaggle/kaggle.sh', folder_name, first_time], check=True)
     # continue with Python code
     print("The script has finished executing.")
     predictions = pd.read_csv(
-        f"static/generated/{folder_name}/output/preds.csv", usecols=["comment_id", "questions", "negative"], lineterminator='\n')
+        f"static/generated/{folder_name}/output/preds.csv", 
+        usecols=["comment_id", "questions", "negative"], lineterminator='\n')
     print(predictions.head(5))
     pred_counts = predictions.negative.value_counts().to_dict()
     quest_counts = predictions.questions.value_counts().to_dict()
@@ -34,6 +42,3 @@ def get_sentiment(comments, folder_name, first_time):
         if quest.size > 0 and quest[0] == 1:
             questions.append(item)
     return quest_counts, pred_counts, negatives, questions
-
-
-
